bf4fcc7f13767f0ccd16df96a80650872f85e2c6
Ekaterina Tokarenko <kate.tokarenko8@gmail.com>
2025-05-11 10:46:15 +0300

    feat(styles): set up base css variables, install scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-function App() {
-  return;
-}
+import "./styles/index.scss";
 
-export default App;
+export const App = () => {
+  return (
+    <div className="app">
+      <div className="app__content">
+        <main className="app__main">123</main>
+        <footer />
+      </div>
+    </div>
+  );
+};
