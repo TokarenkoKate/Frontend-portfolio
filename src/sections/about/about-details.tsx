60716117dbedb6b8c1913474a7c545a4e7a8de9c
Ekaterina Tokarenko <kate.tokarenko8@gmail.com>
2025-05-14 16:20:34 +0300

    fix(styles): fix sections height

--- src/sections/about/about-details.tsx
+++ src/sections/about/about-details.tsx
@@ -5,13 +5,12 @@ export const AboutDetails = () => {
   return (
     <UiVStack className="about__details">
       <UiVStack className="about__section-body">
-        <UiText>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchange
+        <UiText className="about__details-text">
+          I'm a frontend developer with experience building web applications
+          using React, TypeScript, CSS/SCSS, and modern tooling like Webpack and
+          Vite. I’m proficient in state management with Redux, writing
+          well-tested code, and optimizing performance. I’ve also developed
+          cross-platform mobile apps using React Native.
         </UiText>
       </UiVStack>
     </UiVStack>
